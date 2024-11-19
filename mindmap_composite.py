ed3752c0d6bba327171a08328fa898c67bc873c1
charleshahn85 <charleshahn707@gmail.com>
2024-11-19 10:59:41 -0800

    Add files via upload

---
+++ mindmap_composite.py
@@ -0,0 +1,34 @@
+from mindmap_leaf import MindMapLeaf
+import os
+
+class MindMapComposite:
+    def __init__(self, name, shape):
+        self.name = name
+        self.shape = shape
+        self.children = []
+
+    def add(self, child):
+        self.children.append(child)
+
+    def remove(self, child):
+        self.children.remove(child)
+
+    def __str__(self):
+        return self.get_shape_representation().format(self.name)
+
+    def display(self, indent=0):
+        print(" " * indent + str(self))
+        for child in self.children:
+            child.display(indent + 2)
+
+    def get_shape_representation(self):
+        shapes = {
+            "circle": "(({}))",
+            "oval": "({})",
+            "square": "[{}]",
+            "cloud": "){}(",
+            "hexagon": "{{{{{}}}}}",
+            "bang": ")){}(("
+        }
+        return shapes.get(self.shape, "{}")
+
---
+++ test_mindmap_composite.py
@@ -0,0 +1,13 @@
+from mindmap_leaf import MindMapLeaf
+from mindmap_composite import MindMapComposite
+
+root = MindMapComposite("Root", "circle")
+leaf1 = MindMapLeaf("Child 1", "square")
+leaf2 = MindMapLeaf("Child 2", "cloud")
+root.add(leaf1)
+root.add(leaf2)
+
+print(str(root))  # Should display "((Root))"
+root.display()    # Should display root and its children
+
+print("MindMapComposite tests completed!")
